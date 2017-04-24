1f592dec3a7a6cbb575698ee54ba8c4bd047d1fa
Wei Sun <william.sun@leappmusic.com>
2017-04-24 13:37:10 +0800

    customized text color for special dates.

--- PDTSimpleCalendarDemo/PDTSimpleCalendarDemo/CalendarTestViewController.h
+++ PDTSimpleCalendarDemo/PDTSimpleCalendarDemo/CalendarTestViewController.h
@@ -7,4 +7,5 @@
 
 #import <UIKit/UIKit.h>
 @interface CalendarTestViewController : UIViewController
+@property(nonatomic, strong) NSArray *customDates;
 @end
--- PDTSimpleCalendarDemo/PDTSimpleCalendarDemo/CalendarTestViewController.m
+++ PDTSimpleCalendarDemo/PDTSimpleCalendarDemo/CalendarTestViewController.m
@@ -17,6 +17,9 @@
 }
 
 - (void)viewDidLoad {
+
+    [self initCustomDates];
+
     PDTSimpleCalendarViewController *calendarViewController = [[PDTSimpleCalendarViewController alloc] init];
     //This is the default behavior, will display a full year starting the first of the current month
     [calendarViewController setDelegate:self];
@@ -43,5 +46,47 @@
         make.top.equalTo(self.view).offset(64);
         make.left.right.bottom.equalTo(self.view);
     }];
+}
+
+#pragma mark - PDTSimpleCalendarViewDelegate
+
+- (void)simpleCalendarViewController:(PDTSimpleCalendarViewController *)controller didSelectDate:(NSDate *)date
+{
+    NSLog(@"Date Selected : %@",date);
+    NSLog(@"Date Selected with Locale %@", [date descriptionWithLocale:[NSLocale systemLocale]]);
+}
+
+- (BOOL)simpleCalendarViewController:(PDTSimpleCalendarViewController *)controller shouldUseCustomColorsForDate:(NSDate *)date
+{
+    if ([self.customDates containsObject:date]) {
+        return YES;
+    }
+
+    return NO;
+}
+
+- (UIColor *)simpleCalendarViewController:(PDTSimpleCalendarViewController *)controller circleColorForDate:(NSDate *)date
+{
+    return [UIColor clearColor];
+}
+
+- (UIColor *)simpleCalendarViewController:(PDTSimpleCalendarViewController *)controller textColorForDate:(NSDate *)date
+{
+    return [UIColor colorWithRed:251.f/255 green:199.f/255 blue:7.f/255 alpha:1.0];
+}
+
+#pragma mark - Private
+
+//Add 3 custom dates, the 15th for the current & 2 next months.
+- (void)initCustomDates
+{
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitMonth|NSCalendarUnitYear fromDate:[NSDate date]];
+    components.day = 15;
+    NSDate *date1 = [[NSCalendar currentCalendar] dateFromComponents:components];
+    NSDateComponents *addOneMonthComponents = [[NSDateComponents alloc] init];
+    addOneMonthComponents.month =1;
+    NSDate *date2 = [[NSCalendar currentCalendar] dateByAddingComponents:addOneMonthComponents toDate:date1 options:0];
+    NSDate *date3 = [[NSCalendar currentCalendar] dateByAddingComponents:addOneMonthComponents toDate:date2 options:0];
+    self.customDates = @[date1, date2, date3];
 }
 @end
